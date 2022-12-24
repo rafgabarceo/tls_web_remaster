d7ae165953db6ab5287c2081c9fc2f1b80df3e40
unknown <enrico_sebastian_salazar@dlsu.edu.ph>
2022-12-24 08:29:41 +0800

    Added navbar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 //For styling
 import './App.css'; 
+import './Header.css'; 
+import './Body.css'; 
+import './Footer.css'; 
+import './MaterialUi.css';
 
 //Article components
 import App from './App.js';
