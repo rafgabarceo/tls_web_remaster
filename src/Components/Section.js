066bee98c53c8efbb1fec1fbf10c0e1dcef0956e
enricosebastian <enricosebastiansalazar@gmail.com>
2022-12-28 10:40:09 +0800

    Reduced logic for primary articles

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -5,14 +5,12 @@ import SecondaryArticles from './SecondaryArticles.js';
 class Section extends React.Component {
 
     render() {
-        let articles = this.props.articles.map(article => <PrimaryArticle key={`${article.id}`} article={article}/>);
-        
         return (
             <React.Fragment>
                 <h1 className="heading">{this.props.section}</h1>
                 <div className="articles">
                     <div className="primary-article">
-                        {articles[0]}
+                        <PrimaryArticle key={`${this.props.articles[0].id}`} article={this.props.articles[0]}/>
                     </div>
                     <SecondaryArticles articles={this.props.articles.slice(1,5)}/>
                 </div>
