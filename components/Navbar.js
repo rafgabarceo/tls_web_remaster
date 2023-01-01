2abe213876faa564687aab0d406d4bb514a6e6da
enricosebastian <enricosebastiansalazar@gmail.com>
2023-01-01 21:54:59 +0800

    Added dynamic resizing for browsers

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,33 @@
 import Link from "next/link";
 import styles from '../styles/Home.module.scss';
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Navbar() {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleOnClick = () => {
+        setIsClicked(prevState => !prevState);
+    };
+
     return (
         <div className={`${styles.navbar} sticky`}>
             <Link href="/"><img className={`${styles.nav_logo} nav_logo`} alt="TLS logo"/></Link>
-            <ul>
-                <Link href="/section/university"><li>University</li></Link>
-                <Link href="/section/menagerie"><li>Menagerie</li></Link>
-                <Link href="/section/sports"><li>Sports</li></Link>
-                <Link href="/section/vanguard"><li>Vanguard</li></Link>
-                <Link href="/section/opinion"><li>Opinion</li></Link>
-                <Link href="/about"><li>About Us</li></Link>
-            </ul>
-            <img className={styles.search} src="./search.svg" alt="" />
+            {isClicked ? 
+                <ul>
+                    <input type="text" /> 
+                </ul>
+                :
+                <ul>
+                    <Link href="/section/university"><li>University</li></Link>
+                    <Link href="/section/menagerie"><li>Menagerie</li></Link>
+                    <Link href="/section/sports"><li>Sports</li></Link>
+                    <Link href="/section/vanguard"><li>Vanguard</li></Link>
+                    <Link href="/section/opinion"><li>Opinion</li></Link>
+                    <Link href="/about"><li>About Us</li></Link>
+                </ul>
+            }
+            <div className={styles.searchDiv}><img onClick={handleOnClick} className={styles.search} src="./search.svg" alt="" /></div>
         </div>
     );
 
