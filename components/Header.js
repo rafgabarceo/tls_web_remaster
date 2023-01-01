de1c0fe4f3b536f5fbd9fdde87c107526b905623
enricosebastian <enricosebastiansalazar@gmail.com>
2023-01-01 19:25:58 +0800

    Programmed responsiveness of header

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,13 @@ import Navbar from "./Navbar";
 import Link from "next/link";
 import Image from "next/image";
 
+import styles from '../styles/Home.module.scss';
+
 export default function Header() {
     return (
         <>
-            <div className="logo">
-                <Link href="/"><Image src="./tls.svg" width={800} height={200}/></Link>
+            <div className={styles.logo}>
+                <Link href="/"><img alt="TLS logo"/></Link>
             </div>
             <hr />
             <Navbar/>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import styles from '../styles/Home.module.scss';
+import Image from "next/image";
 
 export default function Navbar() {
     return (
-        <div className="navbar">
+        <div className={styles.navbar}>
+            <Link href="/"><img alt="TLS logo"/></Link>
             <ul>
-                <Link href="/section/university"><li>univ</li></Link>
-                <Link href="/section/menagerie"><li>menage</li></Link>
-                <Link href="/section/sports"><li>sports</li></Link>
-                <Link href="/section/vanguard"><li>vanguard</li></Link>
-                <Link href="/section/opinion"><li>opinion</li></Link>
-                <Link href="/about"><li>about</li></Link>
+                <Link href="/section/university"><li>University</li></Link>
+                <Link href="/section/menagerie"><li>Menagerie</li></Link>
+                <Link href="/section/sports"><li>Sports</li></Link>
+                <Link href="/section/vanguard"><li>Vanguard</li></Link>
+                <Link href="/section/opinion"><li>Opinion</li></Link>
+                <Link href="/about"><li>About</li></Link>
             </ul>
         </div>
     );
