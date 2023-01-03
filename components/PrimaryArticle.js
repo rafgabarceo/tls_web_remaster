cd459f1a449b68e10adb086870e1b98504413696
enricosebastian <enricosebastiansalazar@gmail.com>
2023-01-04 00:53:06 +0800

    Improved card visuals

--- components/PrimaryArticle.js
+++ components/PrimaryArticle.js
@@ -2,6 +2,14 @@ import Link from "next/link";
 import styles from '../styles/Home.module.scss';
 
 export default function PrimaryArticle({article}) {
+    const charCountLimit = 150; //Fits 155 ideally, but add allowance
+    let excerptUncleaned = article.excerpt.rendered;
+
+    let endTagIndex = excerptUncleaned.indexOf("</");
+    excerptUncleaned = excerptUncleaned.substring(0,endTagIndex);
+    let lastSpaceIndex = excerptUncleaned.substring(0,charCountLimit).lastIndexOf(" ");
+    let cleanedExcerpt = excerptUncleaned.substring(0,lastSpaceIndex)+"... Read More</p>";
+
     return (
             <div className={styles.primary}>
                 <div className={styles.card}>
@@ -11,7 +19,7 @@ export default function PrimaryArticle({article}) {
                             {/* We have to use divs here and not p, h1, and other text components or else you'll cause hydration errors */}
                             <div className={styles.headline} dangerouslySetInnerHTML={{__html: article.title.rendered}}/>
                             <div className={styles.author}>by me</div>
-                            <div className={styles.snippet} dangerouslySetInnerHTML={{__html: article.excerpt.rendered}}/>
+                            <div className={styles.snippet} dangerouslySetInnerHTML={{__html: cleanedExcerpt}}/>
                         </div>
                     </Link>
                     <img className={styles.img} src={article.jetpack_featured_media_url} />
