25e5814c940adc157bd68c5435b6a37d69f09736
enricosebastian <enricosebastiansalazar@gmail.com>
2023-01-03 23:55:17 +0800

    Added on hover for primary card

--- components/PrimaryArticle.js
+++ components/PrimaryArticle.js
@@ -4,18 +4,26 @@ import styles from '../styles/Home.module.scss';
 export default function PrimaryArticle({article}) {
     return (
             <div className={styles.primary}>
-                <div className={styles.primaryCard}>
-                    <div className={styles.primaryCardShade}/>
-                    <img className={styles.primaryImg} src={article.jetpack_featured_media_url} />
-                    <Link className={styles.primaryLink} href={`/presents/${article.slug}`}>
-                        <div className={styles.cardContainer}>
-                            <h1 className={styles.cardH1} dangerouslySetInnerHTML={{__html: article.title.rendered}}/>
-                            <p className={styles.cardAuthors}>by you</p>
-                            <p className={styles.cardExcerpt} dangerouslySetInnerHTML={{__html: article.excerpt.rendered}}/>
-                        </div>
+                <div className={styles.card}>
+                    
+                    <Link className={styles.articleLink} href={`/presents/${article.slug}`}>
+                        
                     </Link>
+                    <img className={styles.img} src={article.jetpack_featured_media_url} />
                 </div>
             </div>
         
     );
 }
+
+{/* <div className={styles.primaryCard}>
+    <div className={styles.primaryCardShade}/>
+    <img className={styles.primaryImg} src={article.jetpack_featured_media_url} />
+    <Link className={styles.primaryLink} href={`/presents/${article.slug}`}>
+        <div className={styles.cardContainer}>
+            <h1 className={styles.cardH1} dangerouslySetInnerHTML={{__html: article.title.rendered}}/>
+            <p className={styles.cardAuthors}>by you</p>
+            <p className={styles.cardExcerpt} dangerouslySetInnerHTML={{__html: article.excerpt.rendered}}/>
+        </div>
+    </Link>
+</div> */}
